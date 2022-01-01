40541861705e574eb8d0350f1ebed43cd6c7542d
danielgaribi <danielgaribi@mail.tau.ac.il>
2022-01-01 23:43:05 +0200

    [Simulator] merge into one file

--- Simulator/Simulator.h
+++ Simulator/Simulator.h
@@ -9,53 +9,62 @@
 
 int pc;
 uint32_t innerClks; // TODO: fix sprintf from %d
+int diskTimer;
 
 /* Defines and Macros*/
-#define bool                int
-#define TRUE                1
-#define FALSE               0
-#define MAX_NUM_OF_COMMANDS 4096
-#define MEM_SIZE            4096
-#define NUM_OF_OPCODES      22
-#define NUM_OF_REGISTERS    16
-#define NUM_OF_IO_REGISTERS 23
-#define CMD_LENGTH_HEX      12
-#define MEM_LENGTH_HEX      8
-#define BUFFER_SIZE         32 
-#define PIXEL_BUFFER_SIZE   5
-#define MONITOR_SIZE        256
+#define bool                    int
+#define TRUE                    1
+#define FALSE                   0
+#define MAX_NUM_OF_COMMANDS     4096
+#define MEM_SIZE                4096
+#define NUM_OF_OPCODES          22
+#define NUM_OF_REGISTERS        16
+#define NUM_OF_IO_REGISTERS     23
+#define CMD_LENGTH_HEX          12
+#define MEM_LENGTH_HEX          8
+#define BUFFER_SIZE             32 
+#define PIXEL_BUFFER_SIZE       5
+#define MONITOR_SIZE            256
+#define MAX_LINE_LENGTH         500 
+/* Disk Defines:*/
+#define SECTOR_NUMBER           128
+#define SECTOR_SIZE             128
+#define MAX_DISK_LINE_LENGTH    8
+#define DISK_OPERATION_TIME     1024
+#define MAX_LINE_LENGTH         500 
 
-#define SHIFT_IMM2           0
-#define SHIFT_IMM1           12
-#define SHIFT_RM             24
-#define SHIFT_RT             28
-#define SHIFT_RS             32
-#define SHIFT_RD             36
-#define SHIFT_OPCODE         40
+#define SHIFT_IMM2              0
+#define SHIFT_IMM1              12
+#define SHIFT_RM                24
+#define SHIFT_RT                28
+#define SHIFT_RS                32
+#define SHIFT_RD                36
+#define SHIFT_OPCODE            40
 
-#define MASK_IMM2           (uint64_t)  0xfff   <<  SHIFT_IMM2 
-#define MASK_IMM1           (uint64_t)  0xfff   <<  SHIFT_IMM1
-#define MASK_RM             (uint64_t)  0xf     <<  SHIFT_RM
-#define MASK_RT             (uint64_t)  0xf     <<  SHIFT_RT
-#define MASK_RS             (uint64_t)  0xf     <<  SHIFT_RS
-#define MASK_RD             (uint64_t)  0xf     <<  SHIFT_RD
-#define MASK_OPCODE         (uint64_t)  0xff    <<  SHIFT_OPCODE
-#define MASK_12_LOWER_BITS  0xfff
-#define MASK_8_LOWER_BITS   0xff
+#define MASK_IMM2               (uint64_t)  0xfff   <<  SHIFT_IMM2 
+#define MASK_IMM1               (uint64_t)  0xfff   <<  SHIFT_IMM1
+#define MASK_RM                 (uint64_t)  0xf     <<  SHIFT_RM
+#define MASK_RT                 (uint64_t)  0xf     <<  SHIFT_RT
+#define MASK_RS                 (uint64_t)  0xf     <<  SHIFT_RS
+#define MASK_RD                 (uint64_t)  0xf     <<  SHIFT_RD
+#define MASK_OPCODE             (uint64_t)  0xff    <<  SHIFT_OPCODE
+#define MASK_12_LOWER_BITS      0xfff
+#define MASK_8_LOWER_BITS       0xff
 
-#define SET_CMD_VAL(_bin_cmd, _mask, _shift)           ((uint64_t) ((_bin_cmd) & (_mask))) >> (_shift)
+#define SET_CMD_VAL(_bin_cmd, _mask, _shift)                            ((uint64_t) ((_bin_cmd) & (_mask))) >> (_shift)
 
-#define SET_IMM1(_bin_cmd, _cmd)                       (_cmd)->Imm1     =   CONVERT_12_BIT_TO_32_BIT_UNSIGNED(SET_CMD_VAL((_bin_cmd), MASK_IMM1,   SHIFT_IMM1))
-#define SET_IMM2(_bin_cmd, _cmd)                       (_cmd)->Imm2     =   CONVERT_12_BIT_TO_32_BIT_UNSIGNED(SET_CMD_VAL((_bin_cmd), MASK_IMM2,   SHIFT_IMM2))
-#define SET_RM(_bin_cmd, _cmd)                         (_cmd)->RM       =   SET_CMD_VAL((_bin_cmd), MASK_RM,     SHIFT_RM)
-#define SET_RT(_bin_cmd, _cmd)                         (_cmd)->RT       =   SET_CMD_VAL((_bin_cmd), MASK_RT,     SHIFT_RT)
-#define SET_RS(_bin_cmd, _cmd)                         (_cmd)->RS       =   SET_CMD_VAL((_bin_cmd), MASK_RS,     SHIFT_RS)
-#define SET_RD(_bin_cmd, _cmd)                         (_cmd)->RD       =   SET_CMD_VAL((_bin_cmd), MASK_RD,     SHIFT_RD)
-#define SET_OPCODE(_bin_cmd, _cmd)                     (_cmd)->Opcode   =   SET_CMD_VAL((_bin_cmd), MASK_OPCODE, SHIFT_OPCODE)
+#define SET_IMM1(_bin_cmd, _cmd)                                        (_cmd)->Imm1     =  CONVERT_12_BIT_TO_32_BIT_UNSIGNED(SET_CMD_VAL((_bin_cmd), MASK_IMM1,   SHIFT_IMM1))
+#define SET_IMM2(_bin_cmd, _cmd)                                        (_cmd)->Imm2     =  CONVERT_12_BIT_TO_32_BIT_UNSIGNED(SET_CMD_VAL((_bin_cmd), MASK_IMM2,   SHIFT_IMM2))
+#define SET_RM(_bin_cmd, _cmd)                                          (_cmd)->RM       =  SET_CMD_VAL((_bin_cmd), MASK_RM,     SHIFT_RM)
+#define SET_RT(_bin_cmd, _cmd)                                          (_cmd)->RT       =  SET_CMD_VAL((_bin_cmd), MASK_RT,     SHIFT_RT)
+#define SET_RS(_bin_cmd, _cmd)                                          (_cmd)->RS       =  SET_CMD_VAL((_bin_cmd), MASK_RS,     SHIFT_RS)
+#define SET_RD(_bin_cmd, _cmd)                                          (_cmd)->RD       =  SET_CMD_VAL((_bin_cmd), MASK_RD,     SHIFT_RD)
+#define SET_OPCODE(_bin_cmd, _cmd)                                      (_cmd)->Opcode   =  SET_CMD_VAL((_bin_cmd), MASK_OPCODE, SHIFT_OPCODE)
 
 #define READ_REGISTERS_VALUE(_cmd, _rs_value, _rt_value, _rm_value)     _rs_value = registers_values[(_cmd)->RS];\
                                                                         _rt_value = registers_values[(_cmd)->RT];\
                                                                         _rm_value = registers_values[(_cmd)->RM];
+
 #define READ_REGISTERS_VALUE_NO_RM(_cmd, _rs_value, _rt_value)          _rs_value = registers_values[(_cmd)->RS];\
                                                                         _rt_value = registers_values[(_cmd)->RT];
 #define CONVERT_12_BIT_TO_32_BIT_UNSIGNED(x)                            (x >> 11) == 0 ? x : -1 ^ 0xFFF | x
@@ -144,15 +153,12 @@ typedef struct {
 void set_FD_context( char *argv[] );
 void read_imemin_file();
 void read_dmemin_file();
-void read_diskin_file();
-void read_irq2in_file();
 
 void write_dmemout_file();
 void write_regout_file();
 void write_cycles_file();
 void write_diskout_file();
 void write_monitor_file();
-void write_monitor_yuv_file();
 
 void add_to_trace_file(Command *cmd); /** need to add args */
 void add_to_hwregtrace_file(); /** need to add args */
@@ -164,6 +170,19 @@ bool call_action(Command *cmd);
 void parse_cmd_line(char *line, int local_pc);
 bool isLineEmptyOrNoteOnly( char *line );
 void changeMonitor();
+
+void initDisk(void);
+void readFromDisk(void);
+void writeToDisk(void);
+void diskHandler(void);
+
+void updateInterrupts();
+void updateIrq0();
+void updateIrq2();
+int interruptHandler();
+void return_from_interrupt();
+void initInterrupts();
+void initIrq2();
 
 void add (Command *cmd);
 void sub (Command *cmd);
@@ -186,13 +205,5 @@ void sw  (Command *cmd);
 void reti(Command *cmd);
 void in  (Command *cmd);
 void out (Command *cmd);
-
-extern uint32_t registers_values[NUM_OF_REGISTERS];
-extern uint32_t io_registers_values[NUM_OF_IO_REGISTERS];
-extern uint32_t memory[MEM_SIZE];
-extern uint8_t  monitor[MONITOR_SIZE][MONITOR_SIZE];
-extern Command* commands[MAX_NUM_OF_COMMANDS];
-extern FD_Context context;
-extern char* io_registers_names[];
 
 #endif
