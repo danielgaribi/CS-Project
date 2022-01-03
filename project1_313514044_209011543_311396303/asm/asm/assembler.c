d198737f9feebaf0ddb4dc055f73900e170a9da5
Ofer Tlusty <ofertlusty@mail.tau.ac.il>
2022-01-04 01:05:11 +0200

    [Assembler] set fopen back to w+

--- project1_313514044_209011543_311396303/asm/asm/assembler.c
+++ project1_313514044_209011543_311396303/asm/asm/assembler.c
@@ -275,7 +275,7 @@ void print_imemin(char* imemin_file) {
     uint64_t cmd;
     char cmd_string[CMD_LENGTH_HEX + 1];
     FILE* file;
-    assert(fopen_s(&file, imemin_file, "a+") == 0);
+    assert(fopen_s(&file, imemin_file, "w+") == 0);
     assert(file != NULL);
 
     for (i = 0; i < GlobalPC; i++) {
@@ -321,7 +321,7 @@ void print_dmemin(char* dmemin_file) {
     FILE* file;
     char mem_value_string[MEM_LENGTH_HEX + 1];
     get_memory_array(&memory, &size);
-    assert(fopen_s(&file, dmemin_file, "a+") == 0);
+    assert(fopen_s(&file, dmemin_file, "w+") == 0);
     assert(file != NULL);
 
     for (i = 0; i < size; i++) {
